8133a480856714dd7158dc4692360a664f5d7545
Diego Agustin Putrino Chirivino <agussputrii@gmail.com>
2022-09-09 20:37:50 -0300

    User Controller
    * add conditionals in passwords
    * add error msg in partials folder

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -5,7 +5,21 @@ usersCtrl.renderSignUpForm = (req, res) => {
 };
 
 usersCtrl.signup = (req, res) => {
-    res.send('signup');
+    const errors = [];
+    const {name, email, password, confirm_password} = req.body;
+    if (password != confirm_password) {
+        errors.push({text: 'Passwords do not match'});
+    }
+    if (password.length < 4) {
+        errors.push({text: 'Password must be at least 4 characters'});
+    }
+    if (errors.length > 0) {
+        res.render('users/signup', {
+            errors
+        });
+    } else {
+        res.send('signup successfully');
+    }
 };
 
 usersCtrl.renderSigninForm = (req, res) => {

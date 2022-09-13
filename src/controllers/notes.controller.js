dbad67afe49bf0770631377ac59fefcf32769ad6
Diego Agustin Putrino Chirivino <agussputrii@gmail.com>
2022-09-13 17:00:44 -0300

    Id user verification added
    * Users routes security added
    * Required values in notes inputs added

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -1,46 +1,36 @@
 const notesCrtl = {};
 const Note = require('../models/Note'); 
 require('dotenv').config();
+
 //add new notes
 notesCrtl.renderNoteForm = (req, res) => {
     res.render('notes/new-note');
 };
 
 //create new notes
-notesCrtl.createNewNote = (req, res) => {
-    const { title, description } = req.body;
-    const errors = [];	
-    if(!title){
-        errors.push({text: 'Please write a title'});
-    }
-    if(!description){
-        errors.push({text: 'Please write a description'});
-    }
-    if(errors.length > 0){
-        res.render('notes/new-note', {
-            errors,
-            title,
-            description
-        });
-    }
+notesCrtl.createNewNote = async (req, res) => {
+    const { title, description } = req.body;	
     const newNote = new Note({ title, description });
-    newNote.save();
+    newNote.user = req.user.id;
+    await newNote.save();
     console.log(newNote);
     req.flash('success_msg', 'Note Added Successfully');
     res.redirect('/notes');
-}
-;
+};
 
 //Render all notes
 notesCrtl.renderNotes = async (req, res) => {
-    const notes = await Note.find().lean();
+    const notes = await Note.find({user: req.user.id}).sort({createdAt: 'desc'}).lean();
     res.render('notes/all-notes', {notes});
 };
 
 //Edit notes
 notesCrtl.renderEditForm = async (req, res) => {
     const note = await Note.findById(req.params.id).lean();
-    console.log(note);
+    if(note.user != req.user.id){
+        req.flash('error_msg', 'Not Authorized');
+        return res.redirect('/notes');
+    }
     res.render('notes/edit-note', { note });
 };
 
@@ -49,7 +39,7 @@ notesCrtl.updateNote = async (req, res) => {
     const { title, description } = req.body;
     await Note.findByIdAndUpdate(req.params.id, req.body, {title, description});
     req.flash('success_msg', 'Note Updated Successfully');
-    res.redirect('/notes');
+    return res.redirect('/notes');
 };
 
 //Delete Notes
